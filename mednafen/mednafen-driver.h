014578ef523dee8a23ea0440a023573b6fec590e
twinaphex <libretro@gmail.com>
2020-09-19 17:45:25 +0200

    Cleanups

--- mednafen/mednafen-driver.h
+++ mednafen/mednafen-driver.h
@@ -2,24 +2,7 @@
 #define __MDFN_MEDNAFEN_DRIVER_H
 
 #include <stdio.h>
-#include <vector>
-#include <string>
-
-extern std::vector<MDFNGI *>MDFNSystems;
-
-// Call this function as early as possible, even before MDFNI_Initialize()
-bool MDFNI_InitializeModule(void);
-
-/* Sets the base directory(save states, snapshots, etc. are saved in directories
-   below this directory. */
-void MDFNI_SetBaseDirectory(const char *dir);
-
-/* Closes currently loaded game */
-void MDFNI_CloseGame(void);
 
 void MDFN_DispMessage(const char *format, ...);
-#define MDFNI_DispMessage MDFN_DispMessage
-
-uint32 MDFNI_CRC32(uint32 crc, uint8 *buf, uint32 len);
 
 #endif
